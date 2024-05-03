f3f1136341dafc131aabebbe3b89e7a84201e5f5
Vlad Faust <hey@vladfaust.com>
2024-05-03 19:14:50 +0700

    feat(manager): UI improvements

--- packages/manager/src/pages/Giveaways/Show.vue
+++ packages/manager/src/pages/Giveaways/Show.vue
@@ -31,49 +31,60 @@ onMounted(async () => {
 <template lang="pug">
 .flex.flex-col.items-center.gap-y-2.p-3
   h1.text-2xl.font-semibold.tracking-wide.uppercase Giveaway
-  .grid.gap-y-3.gap-x-3.p-4.bg-base-200.rounded-xl.max-w-md.w-full(
+  .flex.p-4.gap-3.items-center.bg-base-200.rounded-xl.max-w-md.w-full.flex-col(
     v-if="giveaway"
-    style="grid-template-columns: repeat(2, auto)"
   )
-    span.attribute.font-mono ID
-    span {{ giveaway.id }}
+    .attribute-wrapper
+      span.attribute-label.font-mono ID
+      span {{ giveaway.id }}
 
-    span.attribute Type
-    span {{ giveaway.type }}
+    .attribute-wrapper
+      span.attribute-label Type
+      span {{ giveaway.type }}
 
-    template(v-if="giveaway.endsAt")
-      span.attribute Ends at
+    .attribute-wrapper(v-if="giveaway.endsAt")
+      span.attribute-label Ends at
       span {{ giveaway.endsAt }}
 
-    template(v-if="giveaway.tokenAddress")
-      span.attribute Token address
+    .attribute-wrapper(v-if="giveaway.tokenAddress")
+      span.attribute-label Token address
       span {{ giveaway.tokenAddress }}
 
-    span.attribute Amount
-    span {{ giveaway.amount }}
+    .attribute-wrapper
+      span.attribute-label Amount
+      span {{ giveaway.amount }}
 
-    span.attribute Receiver count
-    span {{ giveaway.receiverCount }}
+    .attribute-wrapper
+      span.attribute-label Receiver count
+      span {{ giveaway.receiverCount }}
 
-    template(v-if="giveaway.taskUrl")
-      span.attribute Task URL
+    .attribute-wrapper(v-if="giveaway.taskUrl")
+      span.attribute-label Task URL
       span {{ giveaway.taskUrl }}
 
-    span.attribute Status
-    span {{ giveaway.status }}
+    .attribute-wrapper
+      span.attribute-label Status
+      span {{ giveaway.status }}
 
-    span.attribute Participant count
-    span {{ giveaway.participantCount }}
+    .attribute-wrapper
+      span.attribute-label Participant count
+      span {{ giveaway.participantCount }}
 
-    span.attribute Giveaway link
-    a.break-all.font-mono.dz-link-hover.w-full(:href="giveaway.giveawayLink") {{ giveaway.giveawayLink }}
+    .attribute-wrapper
+      span.attribute-label Giveaway link
+      a.break-all.font-mono.dz-link-hover.w-full(:href="giveaway.giveawayLink") {{ giveaway.giveawayLink }}
 
-    span.attribute Top-up link
-    a.break-all.font-mono.dz-link-hover.w-full(:href="giveaway.topUpLink") {{ giveaway.topUpLink }}
+    .attribute-wrapper
+      span.attribute-label Top-up link
+      a.break-all.font-mono.dz-link-hover.w-full(:href="giveaway.topUpLink") {{ giveaway.topUpLink }}
 </template>
 
 <style lang="scss" scoped>
-.attribute {
-  @apply font-bold text-nowrap text-right;
+.attribute-wrapper {
+  @apply flex flex-col items-center text-center;
+}
+
+.attribute-label {
+  @apply font-bold text-xl;
 }
 </style>
--- packages/manager/src/pages/Home.vue
+++ packages/manager/src/pages/Home.vue
@@ -1,3 +1,12 @@
-<template>
-  <h1>Hello, world!</h1>
+<script lang="ts" setup>
+import { routeLocation } from "../router";
+</script>
+
+<template lang="pug">
+.grid.place-items-center.h-screen.p-3
+  .flex.flex-col.items-center.gap-2
+    h1.text-xl.font-semibold.tracking-wide Welcome to Giveaways Manager!
+    RouterLink.dz-btn.dz-btn-primary(
+      :to="routeLocation({ name: 'CreateGiveaway' })"
+    ) Create a giveaway
 </template>
